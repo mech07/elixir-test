42111a2267a6f16d4aa06b7e505fde6273212a60
mech <mechelle.niezas@infinit-o.com>
2024-09-18 17:21:04 +0800

    added a function that will write 1 line for spliting and counting

--- lib/enum_streams_practice.ex
+++ lib/enum_streams_practice.ex
@@ -17,7 +17,7 @@ defmodule FileProcessor do
     file_path
     |> File.stream!()               # Stream the file line by line
     |> Stream.map(&String.length/1) # Get the length of each line
-    |> Enum.max()                   #display the length of the longest line
+    |> Enum.max()                   #display the length of the longest line.
   end
 
   #A longest_line!/1 that returns the contents of the longest line in a file.
@@ -25,7 +25,7 @@ defmodule FileProcessor do
     file_path = ~S(C:\Users\mechelle.niezas\elixer test\numbers\docs\testfile.txt)
     longest_line = file_path
     |> File.stream!()               # Stream the file line by line
-    |> Enum.reduce("", fn line, longest -> #not sure if there is easier than this lol. how cool we can pass the whole function to a variable
+    |> Enum.reduce("", fn line, longest -> #not sure if there is easier than this lol. how cool we can pass the whole codes to a variable
       if String.length(line) > String.length(longest) do
         line
       else
@@ -33,7 +33,7 @@ defmodule FileProcessor do
       end
     end)
 
-    IO.puts("#{longest_line}") # this will display as is when there is "" in the file and not "\
+    IO.puts("#{longest_line}") #String Interpolation this will display as is when there is "" in the file and not "\
   end
 
   #A words_per_line!/1 that returns a list of numbers, with each number representing the word count in a file.
@@ -44,6 +44,15 @@ defmodule FileProcessor do
     |> File.stream!()                    # Stream the file line by line
     |> Stream.map(&String.split/1)        # Split each line into words
     |> Stream.map(&length/1)              # Count the number of words in each line
+    |> Enum.to_list()                     # Collect the results into a list
+  end
+
+  #combine all the stream.map
+  def words_per_line_v2! do
+    file_path = ~S(C:\Users\mechelle.niezas\elixer test\numbers\docs\testfile.txt)
+    file_path
+    |> File.stream!()                    # Stream the file line by line
+    |> Stream.map(&length(String.split(&1)))             # Split each line into words then Count the number of words in each line
     |> Enum.to_list()                     # Collect the results into a list
   end
 end
