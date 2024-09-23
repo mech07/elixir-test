6f07f98d5ae7d99821cfd8f3ba7832a27dac24d4
mech <mechelle.niezas@infinit-o.com>
2024-09-23 18:29:54 +0800

    csv importer

--- lib/todo_builder.ex
+++ lib/todo_builder.ex
@@ -7,6 +7,15 @@ defmodule TodoBuilder do
       fn entry, todo_list_acc -> add_entry(todo_list_acc, entry) end
       )
   end
+
+  #Notice that you can make the lambda definition more compact with the help of the capture operator:
+  def new_compact(entries \\ []) do
+      Enum.reduce(
+      entries,
+      %TodoBuilder{},
+      &add_entry(&2, &1) #Reverses the order of arguments and delegates to add_entry/2
+      )
+  end
   def add_entry(todo_list, entry) do
     entry = Map.put(entry, :id, todo_list.next_id)
     new_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
@@ -64,6 +73,37 @@ defmodule Input do
   def update(todolist) do
     todolist
     |>TodoBuilder.update_entry(1,&Map.put(&1, :date, ~D[2023-12-20]))
+  end
+
+end
+
+defmodule TodoList.CsvImporter do
+  def import(filename) do
+    filename
+    |>read_lines
+    |>TodoBuilder.new()
+  end
+
+  defp read_lines(file_name) do
+    file_name
+    |> File.stream!()  # Stream the file line by line
+    |> Stream.map(&String.trim_trailing(&1, "\n"))  # Clean up each line by removing trailing newline characters
+    |> Stream.map(&parse_row/1)  # Parse each line into a map
+  end
+
+  defp parse_row(row) do
+    [date, title] = String.split(row, ",")
+    %{
+      date: convert_date(date),  # Convert the date from string to Date
+      title: title  # Keep the title as is
+    }
+  end
+
+  defp convert_date(date_string) do
+    case Date.from_iso8601(date_string) do
+      {:ok, date} -> date
+      {:error, _reason} -> nil  # Handle invalid dates as nil or other logic
+    end
   end
 
 end
